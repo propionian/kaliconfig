f9b55bd5244278d6d243b2b57de2663490ce78ee
propionian <106987625+propionian@users.noreply.github.com>
2022-06-19 06:18:57 +0000

    v0.1
    kaliconf

---
+++ setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+echo "[+] Changing MAC.. "
+sudo ifconfig wlan0 down
+sudo macchanger wlan0 -a
+sudo ifconfig eth0 down
+sudo macchanger eth0 -a
+sudo ifconfig eth0 up
+clear
+echo "[+] Updating.. "
+sudo apt update
+clear
+echo "[+] Downloading repo.. "
+sudo wget -P /home/kali "https://protonvpn.com/download/protonvpn-stable-release_1.0.1-1_all.deb" 
+clear
+echo "[+] Installing repo.. "
+sudo apt install /home/kali/protonvpn-stable-release_1.0.1-1_all.deb
+clear
+echo "[+] Updating repo.. "
+sudo apt update
+clear
+echo "[+] Installing VPN.. "
+sudo apt install -y protonvpn
+clear
+echo "[+] Running.. "
+protonvpn-cli login testobomb
+echo "[+] Running.. "
+protonvpn-cli ks --permanent
+echo "[+] Running.. "
+protonvpn-cli c
+clear
+echo "[+] Mounting.. "
+virtualbox /root/kaliconfig/kali.vbox
